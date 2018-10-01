34c333152af27688896f3fe581599f1060984414
munnsmunns <mmunns16@gmail.com>
2018-09-30 20:09:22 -0500

    Modify home component to display one of 3 nuggets, tried to add a background image (WIP)

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,12 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
     templateUrl: 'home.component.html'
 })
-export class HomeComponent {
-    public text: string;
+export class HomeComponent implements OnInit{
+    public nugget: string;
+    public nuggets = ["There are almost 20,000 known species of bees in the world. About 3,500 live in the United States, and in Minnesota, there are probably close to 400.",
+        "Animals that assist plants in their reproduction as pollinators include species of ants, bats, bees, beetles, birds, butterflies, flies, moths, wasps, as well as other unusual animals.",
+        "Keep bee flowers clean - do no treat bee-friendly flowers with pesticides"];
 
     constructor() {
-        this.text = 'Mongo lab';
+
+    }
+
+    ngOnInit() {
+        const index = Math.floor(Math.random() * this.nuggets.length);
+        this.nugget = this.nuggets[index];
     }
 }

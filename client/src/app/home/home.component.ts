9e0a74b7e1146d902786291a3a7b567c7ed0a939
stev0531 <stev0531@morris.umn.edu>
2018-10-17 13:49:39 -0500

    Modify css for styling on home page
    Added the css file to `home-component.ts` so
    it would recognize changes made in the
    `home-component.css` file. Gave a background
    image to title and sub-title section of html.
    
    @munnsmunns

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 
 @Component({
-    templateUrl: 'home.component.html'
+    templateUrl: 'home.component.html',
+    styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit{
+export class HomeComponent implements OnInit {
     public nugget: string;
     public nuggets = ["There are almost 20,000 known species of bees in the world. About 3,500 live in the United States, and in Minnesota, there are probably close to 400.",
         "Animals that assist plants in their reproduction as pollinators include species of ants, bats, bees, beetles, birds, butterflies, flies, moths, wasps, as well as other unusual animals.",

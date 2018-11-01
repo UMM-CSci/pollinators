fd73b8d3574df68a1225903805d4fdfa6717624d
Matt <mmunns16@gmail.com>
2018-11-01 16:26:28 -0500

    Update home component to get the list of nuggets from the server.

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,21 +1,29 @@
 import {Component, OnInit} from '@angular/core';
+import {Nugget} from "./nugget";
+import {HomeService} from "./home.service";
 
 @Component({
     templateUrl: 'home.component.html',
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-    public nugget: string;
-    public nuggets = ["There are almost 20,000 known species of bees in the world. About 3,500 live in the United States, and in Minnesota, there are probably close to 400.",
-        "Animals that assist plants in their reproduction as pollinators include species of ants, bats, bees, beetles, birds, butterflies, flies, moths, wasps, as well as other unusual animals.",
-        "Keep bee flowers clean - do no treat bee-friendly flowers with pesticides"];
+    public nugget: Nugget;
+    public nuggets: Nugget[];
 
-    constructor() {
+    constructor(public homeService: HomeService) {
 
     }
 
     ngOnInit() {
-        const index = Math.floor(Math.random() * this.nuggets.length);
-        this.nugget = this.nuggets[index];
+        this.homeService.getNuggets().subscribe(
+            nuggets => {
+                this.nuggets = nuggets;
+                const index = Math.floor(Math.random() * this.nuggets.length);
+                this.nugget = this.nuggets[index];
+            },
+            err => {
+                console.log(err);
+            }
+        );
     }
 }
